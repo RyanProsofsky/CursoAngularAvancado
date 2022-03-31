d8c87622962ff17b61586d6b7a6f0657b1ed3eea
Ryan Prosofsky <ry.prosofsky@gmail.com>
2022-03-31 19:18:02 -0300

    commit dia 31/03

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; 
+import { InMemoryDataBase } from './in-memory-database';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
